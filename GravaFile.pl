43381906fd545f14785c7b39af8dcb8ca8752518
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2023-04-12 21:06:15 -0300

    Update GravaFile.pl

--- GravaFile.pl
+++ GravaFile.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 print "Content-Type: text/html; charset=utf-8\n\n";
-print ("<h1>Leitura de arquivos nos diretórios</h1>");
+print ("<h1>Gravação de Arquivos</h1>");
 open (WRITE, '>', './Dados/output.txt') or die $!;
 # Grava linha a linha
     print WRITE "Linha 1\n";
